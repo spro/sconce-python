fd49e534ee635fc44b3375715542817382548934
Sean Robertson <sprobertson@gmail.com>
2017-04-14 11:59:15 -0700

    bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sconce',
-      version='0.0.1',
+      version='0.0.2',
       description='Sconce graph helper',
       url='http://github.com/spro/sconce-python',
       author='Sean Robertson',
